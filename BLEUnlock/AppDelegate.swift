d7372706337a4f89d80a32f4310b46edbf9d0dbb
Takeshi Sone <takeshi.sone@gmail.com>
2019-04-21 12:34:15 +0900

    Initial Commit

---
+++ BLEUnlock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BLEUnlock
+//
+//  Created by Takeshi Sone on 2019/04/21.
+//  Copyright © 2019 Takeshi Sone. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
