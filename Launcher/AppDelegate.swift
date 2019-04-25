e4e82f47a02685b0dca4875a0f2ae91281c4e66c
Takeshi Sone <takeshi.sone@gmail.com>
2019-04-25 20:33:44 +0900

    Launcher: check app already running

--- Launcher/AppDelegate.swift
+++ Launcher/AppDelegate.swift
@@ -3,14 +3,19 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let mainId = Bundle.main.bundleIdentifier!.replacingOccurrences(of: ".Launcher", with: "")
+        guard NSRunningApplication.runningApplications(withBundleIdentifier: mainId).isEmpty else {
+            NSApp.terminate(nil)
+            return
+        }
         let path = Bundle.main.bundlePath as NSString
         var components = path.pathComponents
         components.removeLast()
         components.removeLast()
         components.removeLast()
         components.removeLast()
-        let newPath = NSString.path(withComponents: components)
-        NSWorkspace.shared.launchApplication(newPath)
+        let mainPath = NSString.path(withComponents: components)
+        NSWorkspace.shared.launchApplication(mainPath)
         NSApp.terminate(nil)
     }
 }
