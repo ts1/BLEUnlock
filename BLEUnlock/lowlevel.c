ff727159c4a2ffd7fb1d0313182dbe0097ecbe91
Takeshi Sone <takeshi.sone@gmail.com>
2019-04-25 18:13:29 +0900

    Added code for sandbox

--- BLEUnlock/lowlevel.c
+++ BLEUnlock/lowlevel.c
@@ -46,6 +46,16 @@ void wakeDisplay(void)
 
 void lockScreen(void)
 {
+#if 1
+    // Go to lock screen by private API. Doesn't work in Sandbox.
     extern void SACLockScreenImmediate(void);
     SACLockScreenImmediate();
+#else
+    // Go to display sleep. User must set lock screen immediately after sleep in order to lock.
+    io_registry_entry_t reg = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/IOResources/IODisplayWrangler");
+    if (reg) {
+        IORegistryEntrySetCFProperty(reg, CFSTR("IORequestIdle"), kCFBooleanTrue);
+        IOObjectRelease(reg);
+    }
+#endif
 }
