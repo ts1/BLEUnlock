6d8a91df2ac0c5754b3ccbb3bd9df188ea0c2508
Takeshi Sone <takeshi.sone@gmail.com>
2019-06-08 22:13:50 +0900

    Add web links to about box

--- BLEUnlock/AboutBox.swift
+++ BLEUnlock/AboutBox.swift
@@ -5,6 +5,13 @@ private var aboutBox: AboutBox? = nil
 class AboutBox: NSWindowController, NSWindowDelegate {
     @IBOutlet weak var versionLabel: NSTextField!
 
+    @IBAction func visitHomepage(_ sender: Any) {
+        NSWorkspace.shared.open(URL(string: "https://github.com/ts1/BLEUnlock#readme")!)
+    }
+
+    @IBAction func checkReleases(_ sender: Any) {
+        NSWorkspace.shared.open(URL(string: "https://github.com/ts1/BLEUnlock/releases")!)
+    }
     convenience init() {
         self.init(windowNibName: "AboutBox")
     }
