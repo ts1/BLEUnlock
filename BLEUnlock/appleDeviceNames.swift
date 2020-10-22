083f839c76235353a5f6be1275f474a3918a2e32
Takeshi Sone <takeshi.sone@gmail.com>
2020-10-22 19:02:40 +0900

    Add iPhone models

--- BLEUnlock/appleDeviceNames.swift
+++ BLEUnlock/appleDeviceNames.swift
@@ -33,6 +33,11 @@ let appleDeviceNames = [
     "iPhone12,1": "iPhone 11",
     "iPhone12,3": "iPhone 11 Pro",
     "iPhone12,5": "iPhone 11 Pro Max",
+    "iPhone12,8": "iPhone SE 2nd Gen",
+    "iPhone13,1": "iPhone 12 Mini",
+    "iPhone13,2": "iPhone 12",
+    "iPhone13,3": "iPhone 12 Pro",
+    "iPhone13,4": "iPhone 12 Pro Max",
 
     "iPad1,1": "iPad",
     "iPad2,1": "iPad 2",
